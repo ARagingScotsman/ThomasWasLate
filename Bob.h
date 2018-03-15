158c977d4b8bdffa4268005ad6753f69602ec23e
Daniel Campbell <dcampbell606@gmail.com>
2018-03-15 09:48:03 +0000

    Added Bob and Thomas Classes

---
+++ Bob.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "PlayableCharacter.h"
+
+class Bob : public PlayableCharacter
+{
+public:
+	// Constructor
+	Bob();
+	// This will implement the pure virtual function from PlayableCharacter
+	bool virtual handleInput();
+
+}; // End Class Bob
---
+++ Thomas.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "PlayableCharacter.h"
+
+class Thomas : public PlayableCharacter
+{
+public:
+	// Constructor
+	Thomas();
+	// This will implement the pure virtual function from PlayableCharacter
+	bool virtual handleInput();
+
+}; // End Class Thomas
